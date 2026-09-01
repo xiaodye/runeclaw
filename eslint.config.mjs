09a9f416187c7618fcd8cb591efe6a5dcfb3b0c8
lansen <bluewoods.lan@gmail.com>
2026-09-01 17:57:10 +0800

    feat: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
+// @ts-check
+
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
     {
         ignores: [
             'dist/**',
